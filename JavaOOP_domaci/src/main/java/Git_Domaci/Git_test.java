679ada5229f4c24ac3fa2112db210a7c42f37602
Aleksandra Brankovic <aleksandra.brankovic21@gmail.com>
2022-05-13 10:26:27 +0200

    src/main/java/Git_Domaci/Git_test.java

--- JavaOOP_domaci/src/main/java/Git_Domaci/Git_test.java
+++ JavaOOP_domaci/src/main/java/Git_Domaci/Git_test.java
@@ -2,6 +2,6 @@ package Git_Domaci;
 
 public class Git_test {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello worldgit ");
     }
 }
